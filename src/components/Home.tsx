831df6ea9e6083d12138bf088bc0943f75e142ae
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2024-04-25 15:38:22 -0300

    Added leaflet

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,9 @@
 import './componentStyles/home.css';
 
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { TileLayer } from 'react-leaflet/TileLayer';
+import 'leaflet/dist/leaflet.css';
+
 import ReactPlayer from 'react-player/lazy';
 
 function Home( {...props} ) {
@@ -9,8 +13,13 @@ function Home( {...props} ) {
         <div id='homeImg'>
           <h1 id='homeTitle'>Hawai'i Surf Forecasts</h1>
         </div>
-        <div>
-          <ReactPlayer url='https://www.youtube.com/watch?v=VI8Wj5EwoRM' playing muted controls/>
+        <div className='homeSectionSplit'>
+          <ReactPlayer className="liveCam" url='https://www.youtube.com/watch?v=VI8Wj5EwoRM' playing muted controls/>
+          <div>
+            <MapContainer center={[20.600, -157.390]} zoom={6.5}>
+              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </MapContainer>
+          </div>
         </div>
       </div>
     )
