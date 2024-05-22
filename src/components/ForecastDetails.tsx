4138468185d07f1ccc096769609f8f7c88e162f5
John <johnfuhrmeister12@gmail.com>
2024-05-22 00:17:01 -0400

    Python script to get NSBC buoy data

--- src/components/ForecastDetails.tsx
+++ src/components/ForecastDetails.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router';
+import axios from 'axios';
 import './componentStyles/forecastDetails.css';
 
 import firebaseInit from './firebaseConfig';
@@ -22,8 +23,23 @@ function ForecastDetails( {...props} ) {
         setLocation(validLocations[0]);
     }
 
+    const getNDBCData = async () => {
+        const northShoreBuoy = '51201';
+        const endpoint = `https://www.ndbc.noaa.gov/data/realtime2/${northShoreBuoy}.txt`;
+
+        try {
+            await axios.get(endpoint).then((res) => {
+                console.log(res);
+            });
+        } catch(e) {
+            console.log(e);
+        }
+
+    }
+
     useEffect(() => {
         getLocationDetails();
+        getNDBCData();
     }, [])
 
     return (
@@ -34,7 +50,10 @@ function ForecastDetails( {...props} ) {
                     <img id='forecastDetailsMainImg' src={location?.imgLink} alt={location?.name} />
                 </div>
                 <div id='forecastDetailsTopRight'>
-                    
+                    <div id='surfHeightBubble'>
+                        <h2 id='surfBubbleText'>4-5 ft. - Fair</h2>
+                    </div>
+                    <p id='locationDesc'>The Banzai Pipeline is a reef break located in Hawaii, off Ehukai Beach Park in Pupukea on O'ahu's North Shore. Pipeline is known for huge waves that break in shallow water just above a sharp and cavernous reef, forming large, hollow, thick curls of water that surfers can tube ride. There are three reefs at Pipeline in progressively deeper water that activate according to the increasing size of swell.</p>
                 </div>
             </div>
         </div>
